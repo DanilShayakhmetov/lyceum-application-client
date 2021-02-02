2a123f8f1b37ddb6727b2c34e1f2c8d513cc3309
Danil <abraham_lincoln5@rambler.ru>
2021-02-02 04:04:14 +0300

    home fragment

--- app/src/main/java/com/example/lyceum_application_android_client/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/lyceum_application_android_client/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.lyceum_application_android_client.ui.home
 
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -19,6 +20,7 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
 
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -29,25 +31,57 @@ class HomeFragment : Fragment() {
         val session = SessionManager(context);
         super.onCreate(savedInstanceState)
         val pref: SharedPreferences = session.pref
-        val name: String = pref.getString("username", "dsfsdf").toString()
+        val name: String = pref.getString("username", "qwe").toString()
         var user = Users()
         user = handler.getUserByName(name)
         if (user.userName.isNotEmpty()) {
             homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
             val root = inflater.inflate(R.layout.fragment_home, container, false)
-            val textView: TextView = root.findViewById(R.id.text_home)
-            homeViewModel.text.observe(this, Observer {
-                textView.text = user.userName
+            val textViewName: TextView = root.findViewById(R.id.text_name)
+            val textViewFirst: TextView = root.findViewById(R.id.text_firstName)
+            val textViewLast: TextView = root.findViewById(R.id.text_lastName)
+            val textViewMiddle: TextView = root.findViewById(R.id.text_middleName)
+            val textViewEmail: TextView = root.findViewById(R.id.text_email)
+            val textViewClass: TextView = root.findViewById(R.id.text_Class_id)
+
+            homeViewModel.name.observe(this, Observer {
+                textViewName.text = user.userName
             })
+            homeViewModel.first.observe(this, Observer {
+                textViewFirst.text = user.firstName
+            })
+            homeViewModel.last.observe(this, Observer {
+                textViewLast.text = user.lastName
+            })
+            homeViewModel.middle.observe(this, Observer {
+                textViewMiddle.text = user.middleName
+            })
+            homeViewModel.classId.observe(this, Observer {
+                textViewClass.text = user.classId
+            })
+            homeViewModel.email.observe(this, Observer {
+                textViewEmail.text = user.email
+            })
+
             return root
         } else {
             homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
             val root = inflater.inflate(R.layout.fragment_home, container, false)
-            val textView: TextView = root.findViewById(R.id.text_home)
+            val textViewName: TextView = root.findViewById(R.id.text_name)
+            val textViewFirst: TextView = root.findViewById(R.id.text_firstName)
+            val textViewLast: TextView = root.findViewById(R.id.text_lastName)
+            val textViewMiddle: TextView = root.findViewById(R.id.text_middleName)
+            val textViewEmail: TextView = root.findViewById(R.id.text_email)
+            val textViewClass: TextView = root.findViewById(R.id.text_Class_id)
             homeViewModel.text.observe(this, Observer {
-                textView.text = "BITCH"
+                textViewName.text = user.userName
+                textViewFirst.text = user.firstName
+                textViewLast.text = user.lastName
+                textViewMiddle.text = user.middleName
+                textViewEmail.text = user.email
+                textViewClass.text = user.classId
             })
             return root
         }
