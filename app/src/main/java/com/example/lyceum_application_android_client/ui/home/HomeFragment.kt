0d53d83c3b7dbdfe50ab53e7e2986ab01104378a
Danil <abraham_lincoln5@rambler.ru>
2021-02-03 01:10:50 +0300

    basic img 01:11

--- app/src/main/java/com/example/lyceum_application_android_client/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/lyceum_application_android_client/ui/home/HomeFragment.kt
@@ -1,23 +1,21 @@
 package com.example.lyceum_application_android_client.ui.home
 
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.lyceum_application_android_client.DatabaseHelper
-import com.example.lyceum_application_android_client.MainActivity
 import com.example.lyceum_application_android_client.R
 import com.example.lyceum_application_android_client.SessionManager
 import com.example.lyceum_application_android_client.models.Users
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
 
 class HomeFragment : Fragment() {
 
@@ -34,12 +32,15 @@ class HomeFragment : Fragment() {
         val handler: DatabaseHelper = DatabaseHelper(context)
         val session = SessionManager(context);
         val name: String = session.userDetails.get("name").toString()
-        var user = Users()
-        user = handler.getUserByName(name)
+        val user: Users  = handler.getUserByName(name)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val imgView = inflater.inflate(R.layout.image, container, false)
+        root.img_button.setOnClickListener() {
+            showImage(root)
+        }
         if (user.userName.isNotEmpty()) {
             homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
-            val root = inflater.inflate(R.layout.fragment_home, container, false)
             val textViewName: TextView = root.findViewById(R.id.text_name)
             val textViewFull: TextView = root.findViewById(R.id.text_fullName)
             val textViewEmail: TextView = root.findViewById(R.id.text_email)
@@ -67,7 +68,6 @@ class HomeFragment : Fragment() {
         } else {
             homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
-            val root = inflater.inflate(R.layout.fragment_home, container, false)
             val textViewName: TextView = root.findViewById(R.id.text_name)
             val textViewFull: TextView = root.findViewById(R.id.text_fullName)
             val textViewEmail: TextView = root.findViewById(R.id.text_email)
@@ -81,6 +81,10 @@ class HomeFragment : Fragment() {
             return root
         }
 
+    }
+
+    private fun showImage(image: View) {
+        image.image_layout.visibility = View.VISIBLE
     }
 
 }
