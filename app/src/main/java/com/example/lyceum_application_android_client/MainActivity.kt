3cbfa044d8a30844cc29ad38f2be5f5774b7dfd7
Danil <abraham_lincoln5@rambler.ru>
2021-02-20 01:33:41 +0300

    Save user session on hide

--- app/src/main/java/com/example/lyceum_application_android_client/MainActivity.kt
+++ app/src/main/java/com/example/lyceum_application_android_client/MainActivity.kt
@@ -111,8 +111,10 @@ class MainActivity : AppCompatActivity() {
             val name = login_name.text.toString()
             val user = handler.getUserByName(name)
             val schedule = handler.getSchedule(user.classId)
-            session.createLoginSession(login_name.text.toString(), user.id.toString(), schedule[0], schedule[1], schedule[2], schedule[3], schedule[4], "0")
-            Toast.makeText(this, "login ${user.userName} success!",  Toast.LENGTH_SHORT).show()
+            if (!session.isLoggedIn) {
+                session.createLoginSession(login_name.text.toString(), user.id.toString(), schedule[0], schedule[1], schedule[2], schedule[3], schedule[4], "0")
+                Toast.makeText(this, "login ${user.userName} success!",  Toast.LENGTH_SHORT).show()
+            }
             showMain()
             navView.menu.performIdentifierAction(R.id.navigation_home, 0)
         } else {
